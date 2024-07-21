c8dd6d49833b37a0e8b3302355ff2fc5972083d7
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-07-21 19:22:37 +0200

    refactor(): ajout test carroussel

---
+++ src/catalogue site/Advanced/React_Node_JS/server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/api', (req, res) => {
+    res.json({"users": ["user1", "user2", "user3"]});
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
