fe415f9c3be76d7422a887f89dc572f9168df830
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-07-28 18:57:37 +0200

    feat(): utilisation loaders fait main

--- frontend/src/pages/animation.tsx
+++ frontend/src/pages/animation.tsx
@@ -1,14 +1,11 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import { helix } from 'ldrs';
 import Typewriter from 'typewriter-effect';
-import Countdown from "@/components/utils/Countdown";
-import "../styles/animationPageStyle.css"
+import Countdown from '@/components/utils/Countdown';
+import DNAAnimation from '@/components/loaders/DNAAnimation';
 
-const AnimationPage: React.FC = () => {
-    helix.register();
+const Animation: React.FC = () => {
     const router = useRouter();
-
     const [isCountdownComplete, setIsCountdownComplete] = useState(false);
 
     const handleCountdownComplete = () => {
@@ -17,7 +14,7 @@ const AnimationPage: React.FC = () => {
 
     const handleClick = () => {
         if (isCountdownComplete) {
-            router.push("/nouvelle-page");
+            router.push('/nouvelle-page');
         }
     };
 
@@ -25,12 +22,10 @@ const AnimationPage: React.FC = () => {
         <div className="flex flex-col space-y-4 text-center justify-center items-center h-screen text-xl relative bg-gradient-to-b from-[#79A5FC] via-[#2C6EEE] to-[#0C38BD]">
             {!isCountdownComplete && (
                 <>
-                    <l-helix size="45" speed="2.5" color="white"></l-helix>
+                    <DNAAnimation />
                     <Typewriter
                         onInit={(typewriter) => {
-                            typewriter
-                                .typeString('Ready for the FLEO experience')
-                                .start();
+                            typewriter.typeString('Ready for the FLEO experience').start();
                         }}
                         options={{
                             autoStart: true,
@@ -42,15 +37,15 @@ const AnimationPage: React.FC = () => {
             )}
             <span
                 className={`font-Russo rounded-full border-4 flex items-center justify-center text-center text-4xl transition-transform duration-700 ease-in-out cursor-pointer ${
-                    isCountdownComplete ? "transform scale-150 w-48 h-48" : "w-16 h-16"
+                    isCountdownComplete ? 'transform scale-150 w-48 h-48' : 'w-16 h-16'
                 }`}
                 style={{ transformOrigin: 'center' }}
                 onClick={handleClick}
             >
-                {isCountdownComplete ? "Entrez" : <Countdown initialSeconds={5} onComplete={handleCountdownComplete} />}
-            </span>
+        {isCountdownComplete ? 'Entrez' : <Countdown initialSeconds={5} onComplete={handleCountdownComplete} />}
+      </span>
         </div>
     );
-}
+};
 
-export default AnimationPage;
+export default Animation;
