9b076ef7245923dedc24189c5db1510ed7b1c20e
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-07-29 22:11:14 +0200

    feat(): début page catalogue

---
+++ frontend/src/pages/Catalogue.tsx
@@ -0,0 +1,18 @@
+// src/pages/Accueil.tsx
+import React from 'react';
+import NavBar from "@/components/général/NavBar";
+import Footer from "@/components/général/Footer";
+
+const Catalogue: React.FC = () => {
+    return (
+        <div>
+            <NavBar />
+            <main>
+
+            </main>
+            <Footer/>
+        </div>
+    );
+}
+
+export default Catalogue;
