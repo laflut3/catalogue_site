290dbb29a5d1c3c34107f8f6f730f174ce6c1790
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-07-29 15:46:45 +0200

    feat(): deuxieme section acceuil update

--- frontend/src/components/section/Acceuil/DeuxiemeSectionAcceuil.tsx
+++ frontend/src/components/section/Acceuil/DeuxiemeSectionAcceuil.tsx
@@ -5,13 +5,17 @@ import card1 from "@/../public/assets/images/leoCard.png"; // Chemin vers la pre
 import card2 from "@/../public/assets/images/florianCard.png"; // Chemin vers la deuxième carte
 import styles from '@/styles/section/Acceuil/DeuxiemeSectionAcceuilStyle.module.css';
 
-const DeuxiemeSectionAcceuil: React.FC = () => {
+const DeuxiemeSectionAcceuil = () => {
     return (
         <section className={`font-Russo relative min-h-screen flex flex-col items-center justify-center ${styles.back}`}>
             <div className="absolute flex items-center justify-center">
-                <Image src={web} alt="web" className="size-1/8 opacity-30" />
+                <Image src={web} alt="web" className={`${styles.web} opacity-30`}/>
             </div>
-            <h1 className="text-white text-6xl font-bold mb-4 z-10">QUI SOMMES NOUS ?</h1>
+            <div className={`w-full ${styles.marqueeContainer}`}>
+                <h1 className={`text-white text-6xl font-bold mb-32 z-10 ${styles.marquee}`}>
+                    QUI SOMMES NOUS ?
+                </h1>
+            </div>
             <div className="flex justify-between items-center w-full px-10 z-10">
                 <Image src={card1} alt="Léo Torres" className="w-1/4 h-auto border-2" />
                 <Image src={card2} alt="Florian Filloux" className="w-1/4 h-auto border-2" />
