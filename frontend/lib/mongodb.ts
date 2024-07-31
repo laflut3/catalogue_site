cdde7c1513f357cd7dc65205a1bbbf34f565af70
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-07-31 12:29:11 +0200

    feat(): lib mongo

--- frontend/lib/mongodb.ts
+++ frontend/lib/mongodb.ts
@@ -1,43 +1,13 @@
-import mongoose, { ConnectOptions } from 'mongoose';
-
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
-interface MongooseCache {
-    conn: typeof mongoose | null;
-    promise: Promise<typeof mongoose> | null;
-}
-
-declare global {
-    var mongoose: MongooseCache;
-}
-
-let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
-
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connectToDatabase(): Promise<typeof mongoose> {
-    if (cached.conn) {
-        return cached.conn;
+import mongoose from "mongoose";
+const { MONGODB_URI } = process.env;
+export const connectDB = async () => {
+    try {
+        const { connection } = await mongoose.connect(MONGODB_URI as string);
+        if (connection.readyState === 1) {
+            return Promise.resolve(true);
+        }
+    } catch (error) {
+        console.error(error);
+        return Promise.reject(error);
     }
-
-    if (!cached.promise) {
-        const opts: ConnectOptions = {
-            bufferCommands: false,
-        };
-
-        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-            return mongoose;
-        });
-    }
-
-    cached.conn = await cached.promise;
-    return cached.conn;
-}
-
-export default connectToDatabase;
+};
