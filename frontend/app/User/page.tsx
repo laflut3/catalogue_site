f85da256c0ed27a6835c2568b7d89ac1ca1acf4b
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-08-02 11:12:29 +0200

    fix(): page info user

---
+++ frontend/app/User/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from 'react';
+import NavBar from "@/components/général/NavBar";
+import Footer from "@/components/général/Footer";
+import SectionUserInfo from "@/components/section/User/SectionUserInfo";
+
+const Catalogue: React.FC = () => {
+    return (
+        <div>
+            <NavBar />
+            <main>
+                <SectionUserInfo/>
+            </main>
+            <Footer/>
+        </div>
+    );
+}
+
+export default Catalogue;
