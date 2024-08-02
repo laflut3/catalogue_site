e9eb7cbfecc59b475ff471684bc0b6bd156ddebd
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-08-02 16:05:45 +0200

    fix(): donné utilisateur

--- frontend/src/components/section/User/userUtils/UserInfo.tsx
+++ frontend/src/components/section/User/userUtils/UserInfo.tsx
@@ -1,23 +1,30 @@
 "use client";
 
 import React from "react";
-import { signOut } from "next-auth/react";
+import {signOut} from "next-auth/react";
+import UserInitials from "../../../../../Lib/UserLib/composant/UserInitials";
 
 interface UserProps {
     name: string;
     email: string;
-    image: string;
     firstName: string;
     lastName: string;
+    image : string
+    imageVerif: boolean
 }
 
-const UserPage: React.FC<UserProps> = ({ name, email, image, firstName, lastName }) => {
+const UserPage: React.FC<UserProps> = ({name, email,imageVerif, image, firstName, lastName,}) => {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-blue-50">
+        <div className="flex items-center justify-center bg-blue-50">
             <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
                 <h1 className="text-2xl font-semibold mb-4 text-center text-blue-600">User Information</h1>
-                <div className="flex flex-col items-center">
-                    <img src={image} alt={`${name}'s profile`} className="rounded-full w-24 h-24 mb-4 shadow-md"/>
+                <div className="flex flex-col items-center space-y-4">
+                    {!imageVerif ?(
+                        <UserInitials firstName={firstName} lastName={lastName}/>
+                    ) : (
+                        <img src={image} alt={`${name}'s profile`} className="rounded-full w-24 h-24 mb-4 shadow-md"/>
+                        )
+                    }
                     <div className="text-center text-black space-y-2">
                         <p><strong>Nom:</strong> {firstName}</p>
                         <p><strong>Prénom:</strong> {lastName}</p>
@@ -25,7 +32,7 @@ const UserPage: React.FC<UserProps> = ({ name, email, image, firstName, lastName
                         <p><strong>Email:</strong> {email}</p>
                     </div>
                     <button
-                        onClick={() => signOut({ callbackUrl: '/Accueil' })}
+                        onClick={() => signOut({callbackUrl: '/Accueil'})}
                         className="mt-4 p-3 bg-blue-500 text-white rounded shadow hover:bg-blue-600"
                     >
                         Se déconnecter
@@ -33,7 +40,8 @@ const UserPage: React.FC<UserProps> = ({ name, email, image, firstName, lastName
                 </div>
             </div>
         </div>
-    );
+    )
+        ;
 }
 
 export default UserPage;
