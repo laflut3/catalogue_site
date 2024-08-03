5d4f37f02a460d6a942eae13c7bbaf4763a7040d
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-08-03 18:24:51 +0200

    feat(): amélioration filterbar catalogue

--- frontend/src/components/section/Catalogue/CatalogueUtils/CatalogueFilterBar.tsx
+++ frontend/src/components/section/Catalogue/CatalogueUtils/CatalogueFilterBar.tsx
@@ -2,14 +2,32 @@
 
 import React from 'react';
 
-const CatalogueFilterBar = () => {
+interface CatalogueFilterBarProps {
+    onFilterChange: (type: string) => void;
+}
+
+const CatalogueFilterBar: React.FC<CatalogueFilterBarProps> = ({ onFilterChange }) => {
+
+    const handleEcommerce = () => {
+        onFilterChange("E-commerce");
+    };
+
+    const handleVitrine = () => {
+        onFilterChange("Vitrine");
+    };
+
+    const handlePortefolio = () => {
+        onFilterChange("Portfolio");
+    };
+
     return (
-        <div className="flex justify-center mb-8">
-            <nav className="bg-blue-800 text-white rounded-full overflow-hidden flex" style={{ backgroundColor: "#0C388D" }}>
-                <a href="#" className="px-6 py-2 hover:bg-blue-700 border-r border-white">E-commerce</a>
-                <a href="#" className="px-6 py-2 hover:bg-blue-700 border-r border-white">Vitrine</a>
-                <a href="#" className="px-6 py-2 hover:bg-blue-700">Portofolio</a>
-            </nav>
+        <div className={` flex justify-center text-center p-10 pt-0`}>
+            <div className="bg-blue-800 text-white rounded-full overflow-hidden flex justify-center"
+                 style={{backgroundColor: "#0C388D"}}>
+                <button className="px-6 py-2 hover:bg-blue-700 border-r border-white" onClick={handleEcommerce}>E-commerce</button>
+                <button className="px-6 py-2 hover:bg-blue-700 border-r border-white" onClick={handleVitrine}>Vitrine</button>
+                <button className="px-6 py-2 hover:bg-blue-700" onClick={handlePortefolio}>Portfolio</button>
+            </div>
         </div>
     );
 };
