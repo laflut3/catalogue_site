acafaef6bad65541bae4022ea1ac9d418ee34f50
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-08-03 17:33:32 +0200

    feat(): upgrade layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,28 +1,32 @@
-// app/layout.tsx
+"use client";
+
 import React from 'react';
 import '@/../src/styles/Globals.css';
-import {Provider} from "./provider";
+import { Provider } from "./provider";
 import NavBar from "@/components/général/NavBar";
 import Footer from "@/components/général/Footer";
+import { usePathname } from 'next/navigation';
 
 interface RootLayoutProps {
     children: React.ReactNode;
 }
 
 const RootLayout = ({ children }: RootLayoutProps) => {
+    const pathname = usePathname();
+    const showNavAndFooter = pathname !== '/';
+
     return (
         <html lang="en">
         <head>
-            <link rel="icon" href='/fleo-web-reversed.ico'/>
+            <link rel="icon" href='/fleo-web-reversed.ico' />
             <title>Fleo-WEB</title>
         </head>
         <body>
         <Provider>
-            <NavBar />
+            {showNavAndFooter && <NavBar />}
             {children}
-            <Footer />
+            {showNavAndFooter && <Footer />}
         </Provider>
-
         </body>
         </html>
     );
