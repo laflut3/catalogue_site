2acb81494e0b84df7ef62db9617f9b3cfd67d895
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-08-05 18:17:29 +0200

    feat(): Admin page final

--- frontend/app/Admin/page.tsx
+++ frontend/app/Admin/page.tsx
@@ -1,33 +1,31 @@
 "use client";
 
-import {useEffect, useState} from 'react';
-import {useRouter} from 'next/navigation'; // Utiliser 'next/navigation' au lieu de 'next/router'
-import NavBar from '@/components/général/NavBar';
-import Footer from '@/components/général/Footer';
-import AdminForm from '@/components/section/Admin/AdminLinksForm';
-import {useSession} from "next-auth/react";
-import {session} from "next-auth/core/routes";
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation'; // Use 'next/navigation' instead of 'next/router'
+import AdminForm from '@/components/section/Admin/SectionAdmin';
+import { useSession } from "next-auth/react";
 
 const AdminPage: React.FC = () => {
     const router = useRouter();
     const [loading, setLoading] = useState(true);
-    const {data: session, status} = useSession();
+    const { data: session, status } = useSession();
 
     useEffect(() => {
         if (status === "unauthenticated") {
             router.push('/Sign');
-        }
-    }, [status, router]);
-
-    useEffect(() => {
-        if (!session?.user?.isAdmin){
+        } else if (status === "authenticated" && !session?.user?.isAdmin) {
             router.push('/User');
         }
-    }, [router]);
+        setLoading(false); // Ensure loading state is updated
+    }, [status, session, router]);
+
+    if (loading) {
+        return <div>Loading...</div>; // Add a loading state to avoid rendering before checks are complete
+    }
 
     return (
         <main>
-            <AdminForm/>
+            <AdminForm />
         </main>
     );
 };
