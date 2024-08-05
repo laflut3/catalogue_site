86d1c8d0ce05e435f79eb2b36966ca58c6ea49f7
Torres Leo <leo.torres@etu.umontpellier.fr>
2024-08-05 19:06:55 +0200

    feat(): début page about

---
+++ frontend/app/About/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from "react";
+import TitleComponent from "@/components/utils/TitleComponent";
+
+const About: React.FC = () => {
+    return (
+        <main>
+            <TitleComponent title={"A propos"}/>
+        </main>
+    );
+}
